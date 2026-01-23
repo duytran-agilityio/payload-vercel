78e38be4b244ba7c384590892289605645f3eadc
Duy Tran <duytran@asnet.com.vn>
2026-01-23 21:59:25 +0700

    feat(): add user roles

---
+++ src/migrations/20260123_145902_user_roles.ts
@@ -0,0 +1,34 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   CREATE TYPE "public"."enum_users_roles" AS ENUM('admin', 'editor', 'user');
+  CREATE TABLE "users_roles" (
+  	"order" integer NOT NULL,
+  	"parent_id" integer NOT NULL,
+  	"value" "enum_users_roles",
+  	"id" serial PRIMARY KEY NOT NULL
+  );
+  
+  ALTER TABLE "users" ADD COLUMN "avatar_id" integer;
+  ALTER TABLE "users" ADD COLUMN "active" boolean DEFAULT true;
+  ALTER TABLE "users" ADD COLUMN "name" varchar;
+  ALTER TABLE "users_roles" ADD CONSTRAINT "users_roles_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+  CREATE INDEX "users_roles_order_idx" ON "users_roles" USING btree ("order");
+  CREATE INDEX "users_roles_parent_idx" ON "users_roles" USING btree ("parent_id");
+  ALTER TABLE "users" ADD CONSTRAINT "users_avatar_id_media_id_fk" FOREIGN KEY ("avatar_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  CREATE INDEX "users_avatar_idx" ON "users" USING btree ("avatar_id");`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "users_roles" DISABLE ROW LEVEL SECURITY;
+  DROP TABLE "users_roles" CASCADE;
+  ALTER TABLE "users" DROP CONSTRAINT "users_avatar_id_media_id_fk";
+  
+  DROP INDEX "users_avatar_idx";
+  ALTER TABLE "users" DROP COLUMN "avatar_id";
+  ALTER TABLE "users" DROP COLUMN "active";
+  ALTER TABLE "users" DROP COLUMN "name";
+  DROP TYPE "public"."enum_users_roles";`)
+}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260122_085302 from './20260122_085302';
+import * as migration_20260123_145902_user_roles from './20260123_145902_user_roles';
 
 export const migrations = [
   {
     up: migration_20260122_085302.up,
     down: migration_20260122_085302.down,
-    name: '20260122_085302'
+    name: '20260122_085302',
+  },
+  {
+    up: migration_20260123_145902_user_roles.up,
+    down: migration_20260123_145902_user_roles.down,
+    name: '20260123_145902_user_roles'
   },
 ];
